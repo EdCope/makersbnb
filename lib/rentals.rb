e5703ee5b53daf1502d0f414e664d3e57014f87f
Ed <ed321@hotmail.co.uk>
2022-02-22 12:57:33 +0000

    added remaining columns to  test and model
    Co-authored-by: AliCo-Hash <AliCo-Hash@users.noreply.github.com>

--- lib/rentals.rb
+++ lib/rentals.rb
@@ -1,8 +1,12 @@
 class Rentals 
-  attr_reader :title
+  attr_reader :title, :id, :rental_description, :price, :contact_details
 
-  def initialize(title:)
+  def initialize(title:, id:, rental_description:, price:, contact_details:)
     @title = title
+    @id = id
+    @rental_description = rental_description
+    @price = price.to_f
+    @contact_details = contact_details
   end
 
 
@@ -15,7 +19,11 @@ class Rentals
 
     result = connection.exec("SELECT * FROM rentals")
 
-    result.map { |rental| Rentals.new(title: rental["title"]) }
+    result.map { |rental| Rentals.new(title: rental["title"], 
+    id: rental["id"],
+    rental_description: rental["rental_description"],
+    price: rental["price"],
+    contact_details: rental["contact_details"]) }
   end
   
 end
--- spec/rentals_spec.rb
+++ spec/rentals_spec.rb
@@ -6,15 +6,19 @@ describe Rentals do
 
       connection = PG.connect(dbname: 'makersbnb_test')
       
-      connection.exec("INSERT INTO rentals (title) VALUES ('24 Zoo lane');")
+      rental = connection.exec("INSERT INTO rentals (title, rental_description, price, contact_details) 
+      VALUES ('24 Zoo lane', 'rdt1', 99.99, 'cdt1');")
       connection.exec("INSERT INTO rentals (title) VALUES('Archies house in Balamory');")
 
       rentals = Rentals.all
 
       expect(rentals.length).to eq 2
+      # expect(rentals.first).to eq rental.id
       expect(rentals.first).to be_a Rentals
       expect(rentals.first.title).to eq '24 Zoo lane'
-
+      expect(rentals.first.rental_description).to eq 'rdt1'
+      expect(rentals.first.price).to eq 99.99
+      expect(rentals.first.contact_details).to eq 'cdt1'
     end
   end
 end
