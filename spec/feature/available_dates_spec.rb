1b4d2e3ee461aeea0d1859618e21f488851ad8e6
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-02-25 11:54:12 +0000

    debugging css layout

--- spec/feature/available_dates_spec.rb
+++ spec/feature/available_dates_spec.rb
@@ -1,6 +1,7 @@
 feature 'available dates' do
   scenario 'allows host to input available dates in form' do
     register_and_signin
+    click_button('My account')
     click_button('List a property')
     expect(current_path).to eq '/add_rental'
     expect(page).to have_field('title')
